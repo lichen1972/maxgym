f8e181c2571946fe6e8be49cb1b001cd73fabfbb
lichen1972 <lichen.ng@gmail.com>
2026-09-24 16:27:28 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'maxgym-';
-const CACHE = PREFIX + 'v10';
+const CACHE = PREFIX + 'v11';
 const FILES = ['./', 'index.html', 'manifest.json', 'icons/icon-192.png', 'icons/icon-512.png',
   'icons/icon-maskable-192.png', 'icons/icon-maskable-512.png', 'icons/apple-touch-icon.png'];
 
